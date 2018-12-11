b55729afa3597844356c2aedbc66ea7614800082
burakon <gotou015@gmail.com>
2018-12-11 20:11:59 +0900

    05_assets追加と必要なインスタンスの宣言

--- FlappyBird/FlappyBird/GameScene.swift
+++ FlappyBird/FlappyBird/GameScene.swift
@@ -9,7 +9,17 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+// SKPhysicsContactDelegateを追加、物体と物体がぶつかった時に呼ばれるメソッド群
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    
+    // 飛ぶ鳥 bird を作る
+    var bird = SKSpriteNode()
+    // ゲームオーバー時の画像を作る
+    var gameOverImage = SKSpriteNode()
+    // タップ時に鳥が跳ねる音
+    let jumpSound = SKAction.playSoundFileNamed("sound.mp3", waitForCompletion: false)
+    // バックグラウンド音 https://maoudamashii.jokersounds.com/ 自分の好きな曲使ってください
+    let backSound = SKAction.playSoundFileNamed("buckSound.mp3", waitForCompletion: false)
     
     override func didMove(to view: SKView) {
     }
